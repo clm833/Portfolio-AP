e59880c40c850551b69157bf45c6fbd8f62aa2e2
Cristian_M <clm.833@gmail.com>
2021-12-25 02:26:17 -0300

    maquetado v0.01

---
+++ Maquetado/main.js
@@ -0,0 +1,21 @@
+loginBtn = document.getElementById('login-btn');
+editBtns = document.getElementsByClassName('edit-btn');
+console.log(editBtns);
+loginBtn.addEventListener('click', (e) => {
+    if (loginBtn.classList.contains('fa-sign-in-alt')) {
+        loginBtn.classList.replace('fa-sign-in-alt', 'fa-sign-out-alt');
+
+        for (let i = 0; i < editBtns.length; i++) {
+            const element = editBtns[i];
+            element.classList.add('active');
+        }
+    }
+    else if (loginBtn.classList.contains('fa-sign-out-alt')) {
+        loginBtn.classList.replace('fa-sign-out-alt', 'fa-sign-in-alt');
+        
+        for (let i = 0; i < editBtns.length; i++) {
+            const element = editBtns[i];
+            element.classList.remove('active');
+        }
+    }
+});
